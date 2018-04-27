3168e0a8ec0af26177463fcf234ba65aa61b3df9
jaywelborn <jesse.welborn@gmail.com>
2018-04-28 00:06:34 +0200

    added components for bingo cards and squares, got them to display properly from card detail URL, and wrote check victory function for bingocards

---
+++ src/Components/Cards/BingoCard/index.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+
+import Square from './Square';
+
+
+let BOARD_SIZE = 5;
+
+export default class BingoCard extends Component {
+
+  constructor(props){
+    super(props);
+    let card = this.props.card;
+    let squares = [];
+    let count = 0;
+
+    // board rows
+    for (let i = 0; i < BOARD_SIZE; i++){
+      squares[i] = [];
+      // board columns
+      for (let j = 0; j < BOARD_SIZE; j++) {
+        if (i === 2 && j === 2) { // place free space in the middle
+          squares[i][j] = {text: card.free_space,
+                           clicked: false}
+        } else {
+          squares[i][j] = {text: this.props.squares[count].text,
+                           clicked: false}
+          count ++;
+        }
+      }
+    }
+
+    this.state = {
+        squares: squares,
+    }
+  }
+
+  componentWillMount() {
+
+  }
+
+  render() {
+    let card = this.props.card;
+    let board = this.state.squares.map((row, i) => {
+      return (<tr key={i}>{row.map((square, j) => {
+        return <Square data={square.text} key={3 * i + j} />
+      })}</tr>)
+    });
+
+    return (
+      <table className="card bingo-card">
+        <thead><tr><th colSpan="5"><h1>{card.title}</h1></th></tr></thead>
+        <tbody>
+          {board}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+function checkVictory(squares) {
+
+  // initialize diagonal
+  let diagonalOne = true;
+  let diagonalTwo = true;
+  let horizontal, vertical;
+
+  for (let i = 0; i < squares.length; i++) {
+    horizontal = true;
+    vertical = true;
+
+    // go through a horizontal and vertical row
+    for (let j = 0; j < squares.length; j++) {
+      // check next square horizontally
+      if (!squares[i][j].clicked) {
+        horizontal = false;
+      }
+
+      // check next square vertically
+      if (!squares[j][i].clicked) {
+        vertical = false;
+      }
+    }
+
+    // go ahead and return if horizontal or vertical stayed true
+    if (horizontal || vertical) {
+      return true;
+    }
+
+    // check both diagonals
+    if (!squares[i][i].clicked) {
+      diagonalOne = false;
+    }
+    if (!squares[i][squares.length - i - 1].clicked) {
+      diagonalTwo = false;
+    }
+  }
+
+  // if any victory status is true now, they've won
+  return (diagonalOne || diagonalTwo || horizontal || vertical);
+}
--- src/Components/Cards/Detail.js
+++ src/Components/Cards/Detail.js
@@ -1,14 +1,43 @@
 import React, { Component } from 'react';
 
-import {Link} from 'react-router-dom';
+import BingoCard from './BingoCard/'
 
+let apiRoot = '';
+if (window.location.href.includes('localhost')) {
+  apiRoot = 'http://localhost:8000/api/';
+}
 
 export default class Detail extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  componentWillMount() {
+    fetch(apiRoot +
+         'cards/' +
+         this.props.match.params.id +
+         '.json', {
+      method: 'get',
+      headers: {},
+      mode: 'cors',
+    }).then(response => response.json())
+    .then(card => {
+      this.setState({card: card, squares: card.squares});
+    });
+  }
+
   render() {
     return (
       <div>
-        <h1>Detail</h1>
-        <Link to="/cards">Card List</Link>
+        {
+          this.state.card ?
+          <BingoCard card={this.state.card} squares={this.state.squares}/>
+          :
+          <h3>Loading...</h3>
+        }
       </div>
     );
   }
