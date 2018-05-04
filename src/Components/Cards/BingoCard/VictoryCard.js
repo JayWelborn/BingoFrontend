acd458b98d6ec4c9c84b2b4c365392fde810dd08
jaywelborn <jesse.welborn@gmail.com>
2018-05-04 22:41:35 +0300

    added audio and image to victory modal

--- src/Components/Cards/BingoCard/VictoryCard.js
+++ src/Components/Cards/BingoCard/VictoryCard.js
@@ -11,7 +11,9 @@ export default class VictoryCard extends Component {
     return (
       <div class='victory'>
         <h1>Victory!</h1>
+        <img src="/images/svg/party.svg" />
         <h5><Link to='/cards/'>More BingoCards</Link></h5>
+        <audio src="/audio/victory.mp3" autoplay="true"></audio>
       </div>
     );
   }
