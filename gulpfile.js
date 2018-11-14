0febb59a73ed19f031f453d9af12239798653919
jaywelborn <jesse.welborn@gmail.com>
2018-11-14 15:12:41 -0500

    Security Patches, Upgraded to Gulp 4.0

--- gulpfile.js
+++ gulpfile.js
@@ -6,22 +6,25 @@ var sass         = require('gulp-sass');
 var sourcemaps   = require('gulp-sourcemaps');
 
 // Process sass files and output prefixed css
-gulp.task('styles', function(done) {
-    gulp.src('./src/Styles/main.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(concat('main.css'))
-        .pipe(sourcemaps.init())
-        .pipe(postcss([autoprefixer() ]))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./src/'));
+function styles() {
+    return (gulp
+            .src('./src/Styles/main.scss')
+            .pipe(sass().on('error', sass.logError))
+            .pipe(concat('main.css'))
+            .pipe(sourcemaps.init())
+            .pipe(postcss([autoprefixer() ]))
+            .pipe(sourcemaps.write('.'))
+            .pipe(gulp.dest('./src/'))
+            );
+};
 
-    done();
-});
 
-// Tell gulp to execute 'styles' every time a sass file changes
-gulp.task('watch', function() {
-    gulp.watch('**/*.{sass,scss}', ['styles']);
-});
+// // Tell gulp to execute 'styles' every time a sass file changes
+function watch() {
+    styles();
+    gulp.watch('**/*.{sass,scss}', styles);
+};
 
-// Start django server and start watching sass files
-gulp.task('default', ['watch']);
+gulp.task('default', function() {
+    watch();
+})
